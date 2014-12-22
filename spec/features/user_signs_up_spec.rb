3b5f506e65a3b01f1a19b5f8f169a5dc5e38c879
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-22 14:32:10 -0500

    Add test for properly formatted email

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -9,7 +9,7 @@ feature "Input a Board Game", %q(
   - [ x ] There is a link to 'Sign Up' on the homepage.
   - [ x ] If I fill in my first name, last name, email, password, and password confirmation correctly, I am greeted with a confirmation message that my account has been created.
   - [ x ] If the password and password confirmation fields do not match, I am given an error message.
-  - [ ] If my email already exists in the database, I am given a message that tells me I have already registered.
+  - [ x ] If my email already exists in the database, I am given a message that tells me I have already registered.
   - [ ] If my email is not formatted correctly, I am given an error message.
 ) do
   scenario "user provides valid information" do
@@ -61,4 +61,18 @@ feature "Input a Board Game", %q(
     expect(page).to have_content "Email has already been taken"
   end
 
+  scenario "improperly formatted email" do
+
+    visit root_path
+    click_on "Sign Up"
+
+    fill_in "First Name", with: "William"
+    fill_in "Last Name", with: "Mahoney"
+    fill_in "Email", with: "wm.j.mahoney"
+    fill_in "Password", with: "supersecret"
+    fill_in "Password confirmation", with: "supersecret"
+    click_on "Sign up"
+
+    expect(page).to have_content "Email is invalid"
+  end
 end
